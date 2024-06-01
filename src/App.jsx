809907208c2151aa289fac93b58027e1de924fbf
harsh khanagwal <harshkhanagwall29@gmail.com>
2024-06-01 22:00:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <header>
+
+      </header>
+   
+
+      <div className="main">
+
+        <nav>
+          hii
+        </nav>
+        <div className="container guide-container">
+
+          <div className="content-area">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere excepturi maxime facilis aperiam earum dignissimos eveniet beatae minus laboriosam molestias, reiciendis impedit quidem blanditiis, incidunt esse necessitatibus, amet enim delectus ex illum dolores temporibus? Beatae minus unde accusamus vel impedit, non placeat error dolorem fuga dolore autem fugiat quod minima. Quidem voluptas obcaecati iure aliquam unde debitis praesentium quasi consequatur minima saepe aliquid dolores itaque natus laborum, minus vitae, ipsum inventore fuga tempora facilis quisquam perferendis, blanditiis cupiditate corrupti! Deserunt laudantium autem alias consectetur. Amet autem laboriosam dolores maxime nihil molestiae harum quas ab nesciunt ad, ducimus asperiores, sed suscipit quae omnis nemo consectetur. Dolore facilis voluptates dicta enim aperiam quae, ea vel quisquam ullam, cum totam maiores ratione praesentium et corporis repudiandae laudantium saepe amet distinctio harum vero. Corrupti obcaecati quaerat eius impedit ut repellendus velit vel error in odit odio numquam, qui doloribus doloremque, hic, animi magnam eaque? Illum officia provident voluptatum neque repellat cumque sit illo labore in vero? Blanditiis excepturi provident fuga. Atque excepturi eligendi architecto, eveniet magni voluptas omnis tempore assumenda cumque id aperiam rerum maiores provident pariatur, illum porro quisquam. Accusamus amet expedita ducimus laborum quaerat maxime? Quas ducimus quo deleniti, dignissimos incidunt praesentium! Nulla sed autem rerum ea excepturi natus culpa, odit minus cum, aliquam laborum nam dicta animi rem neque officiis in eum commodi tempora maiores eos voluptatem? Cum nemo hic repudiandae ut possimus nobis nihil ab, incidunt quam autem qui eligendi dolorem tempore veniam quasi consequatur rem error quis reiciendis sunt illum aliquam iure! Laudantium voluptatum ducimus, enim eius, adipisci voluptas eum commodi voluptates, recusandae harum libero quasi cum expedita distinctio aut iste mollitia nisi explicabo. Ipsum exercitationem quo, explicabo optio doloribus tenetur natus maiores voluptatem dicta tempora eligendi odit incidunt dolorem perspiciatis fugit nobis. Porro pariatur soluta laboriosam illum amet mollitia id. Accusamus iste voluptate maiores quasi ea iure, magnam sapiente, inventore mollitia magni ipsum. Facilis alias dicta earum, officiis consequatur, aspernatur aut, officia nisi nam veritatis delectus. Incidunt minima dolores eos nobis omnis, consequatur aliquid dolorum? Commodi, laborum ipsam quia deleniti ex optio vel totam consequatur maiores mollitia, labore, atque nobis odit aliquid similique iusto aut expedita quasi nam fuga? Quasi nulla assumenda aspernatur, necessitatibus magni ex. Dolor labore consequuntur recusandae doloribus modi aliquid rerum deleniti maxime, veritatis vel perferendis! Veniam, nobis. Totam accusamus architecto asperiores? Quos consectetur illo ipsum unde corporis assumenda vel quia sunt distinctio cupiditate? Natus!
+
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <>
+        <header>
+            
+        </header>
+    </>
+  )
+}
+
+export default Header
