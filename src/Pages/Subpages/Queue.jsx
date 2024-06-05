c48503000fd3e1825dca9b8787202dd1c5c8b6b0
harsh khanagwal <harshkhanagwall29@gmail.com>
2024-06-05 17:53:58 +0530

    adding queue page

---
+++ src/Pages/Subpages/Queue.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Codebx from '../../Components/Codebx/Codebx'
+import Highlight from '../../Components/highlight/Highlight';
+
+
+const Queue = () => {
+    return (
+        <>
+            <div className="container guide-container">
+
+                <div className="content-area">
+
+                    <h1></h1>
+                
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Queue
