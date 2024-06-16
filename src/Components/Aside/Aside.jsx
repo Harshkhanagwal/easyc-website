178152f3b3aad6693f9dd80f140514419dec5605
harsh khanagwal <harshkhanagwall29@gmail.com>
2024-06-16 21:41:53 +0530

    fixing scroll behaviour on page change

--- src/Components/Aside/Aside.jsx
+++ src/Components/Aside/Aside.jsx
@@ -9,9 +9,9 @@ const Aside = (props) => {
 
     const { SetAsideMenu, asideMenu, SetLogo } = useContext(Globalcontext);
 
-   useEffect(() => {
-    SetLogo(false)
-   }, SetLogo)
+    useEffect(() => {
+        SetLogo(false)
+    }, SetLogo)
 
     return (
         <>
@@ -27,13 +27,34 @@ const Aside = (props) => {
 
                     <nav>
                         <ul>
-                            <li onClick={() => SetAsideMenu(!asideMenu)}><NavLink to={'/guide/overview'}>Overview</NavLink></li>
-                            <li onClick={() => SetAsideMenu(!asideMenu)}><NavLink to={'/guide/download'}>Download</NavLink></li>
-                            <li onClick={() => SetAsideMenu(!asideMenu)}><NavLink to={'/guide/installation'}>Installation</NavLink></li>
-                            <li onClick={() => SetAsideMenu(!asideMenu)}><NavLink to={'/guide/array'}>Array</NavLink></li>
-                            <li onClick={() => SetAsideMenu(!asideMenu)}><NavLink to={'/guide/linkedlist'}>Linked List</NavLink></li>
-                            <li onClick={() => SetAsideMenu(!asideMenu)}><NavLink to={'/guide/stack'}>Stack</NavLink></li>
-                            <li onClick={() => SetAsideMenu(!asideMenu)}><NavLink to={'/guide/queue'}>Queue</NavLink></li>
+                            <li onClick={() => {
+                                SetAsideMenu(!asideMenu)
+                                window.scrollTo(0, 0)
+                            }}><NavLink to={'/guide/overview'}>Overview</NavLink></li>
+                            <li onClick={() => {
+                                SetAsideMenu(!asideMenu)
+                                window.scrollTo(0, 0)
+                            }}><NavLink to={'/guide/download'}>Download</NavLink></li>
+                            <li onClick={() => {
+                                SetAsideMenu(!asideMenu)
+                                window.scrollTo(0, 0)
+                            }}><NavLink to={'/guide/installation'}>Installation</NavLink></li>
+                            <li onClick={() => {
+                                SetAsideMenu(!asideMenu)
+                                window.scrollTo(0, 0)
+                            }}><NavLink to={'/guide/array'}>Array</NavLink></li>
+                            <li onClick={() => {
+                                SetAsideMenu(!asideMenu)
+                                window.scrollTo(0, 0)
+                            }}><NavLink to={'/guide/linkedlist'}>Linked List</NavLink></li>
+                            <li onClick={() => {
+                                SetAsideMenu(!asideMenu)
+                                window.scrollTo(0, 0)
+                            }}><NavLink to={'/guide/stack'}>Stack</NavLink></li>
+                            <li onClick={() => {
+                                SetAsideMenu(!asideMenu)
+                                window.scrollTo(0, 0)
+                            }}><NavLink to={'/guide/queue'}>Queue</NavLink></li>
                         </ul>
                     </nav>
                     <hr />
